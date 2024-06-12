0df7365439f7149ce4079b11383dcc50ae328f68
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2024-06-12 17:02:14 -0400

    added dock component

---
+++ components/Dock.jsx
@@ -0,0 +1,127 @@
+import Image from "next/image";
+import styles from "../styles/Dock.module.css";
+
+function Dock() {
+  return (
+    <div id="container" className={styles.container}>
+      <div id="dock">
+        <ul>
+          <li className={styles.dockItem}>
+            <span>Finder</span>
+            <a href="#one">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224838/Finder_kmiknb.png"
+                alt="Finder"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Applications</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224836/Applications_Folder_l8g5jy.png"
+                alt="Applications"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Calendar</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224839/iCal_tzwfww.png"
+                alt="Calendar"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Web Browser</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224840/Internet_Explorer_t9xkdj.png"
+                alt="Chrome"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>iTunes</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224841/iTunes_2_fmevlt.png"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Photoshop</span>
+            <a href="#ps-one">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224843/Preview_smriqk.png"
+                alt="Photos"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Premiere</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224840/iMovie_HD_idbldj.png"
+                alt="Premiere"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Text Edit</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224845/TextEdit_gs86il.png"
+                alt="After Effects"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>Trash Can</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224845/Trash_empty_vepvhm.png"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>|</span>
+            <a href="#">|</a>
+          </li>
+          <li className={styles.dockItem}>
+            <span>System Preferences</span>
+            <a href="#">
+              <Image
+                src="https://res.cloudinary.com/dtps5ugbf/image/upload/v1718224845/System_Prefrences_hjobyq.png"
+                alt="System Preferences"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Dock;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "icons.iconarchive.com",
+      "blog.codepen.io",
+      "res.cloudinary.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
